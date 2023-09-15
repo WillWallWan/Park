3ea124474eef134b59cd27d7c0c7b5d04479821f
WillWallWan <>
2023-09-15 02:22:10 -0400

    Update Park.py

--- Park.py
+++ Park.py
@@ -14,20 +14,20 @@ def park():
 
     
     # Initialize a wait object with a timeout of 10 seconds
-    # wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10)
 
 
     driver.get('https://my.nyc.flowbirdapp.com/#/Parking?panel=login')
 
-    time.sleep(5)
+    
 
     try:
         # Using waits before finding elements to ensure that they are loaded and interactable
-        username_field = wait.until(EC.element_to_be_clickable((By.ID, "w-input--8506283350331578")))
+        username_field = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@type="text" and @size="20"]')))
         username_field.send_keys("willy-wan@hotmail.com")
         
         # Finding the password field and sending the password
-        password_field = wait.until(EC.element_to_be_clickable((By.ID, "w-input--8861484250143397")))
+        password_field = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@type="password" and @size="20"]')))
         password_field.send_keys("Iluvrobin123321")
         
         # Finding the login button and clicking it
@@ -38,13 +38,17 @@ def park():
     except Exception as e:
         print("Error during login:", str(e))
 
+    time.sleep(1)
+
     zone_code = "413279"
-    
-    zone_input = wait.until(EC.element_to_be_clickable((By.ID, 'w-input--9356375178080452')))
+
+    zone_input_xpath = '//input[@type="text" and @size="20"]'
+    zone_input = wait.until(EC.element_to_be_clickable((By.XPATH, zone_input_xpath)))
     zone_input.send_keys(zone_code)
 
     start_parking_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.button-primary.margin-bottom-10-important')))
     start_parking_button.click()
+
     
     # Adding a delay before closing the driver
     time.sleep(5)
